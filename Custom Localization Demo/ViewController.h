d2e9941dbc0b61e32c699701d92af75d38c91f5b
TEKNIP <teknip@TEKNIPs-MacBook-Pro.local>
2015-08-16 10:50:53 +0400

    Initial Commit

---
+++ Custom Localization Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Custom Localization Demo
+//
+//  Created by TEKNIP on 8/16/15.
+//  Copyright (c) 2015 Temp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
